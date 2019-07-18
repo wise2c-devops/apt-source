28bdfa00fad816913fae210e3de6702a72c0af1c
Alan Peng <peng.alan@gmail.com>
2019-07-19 00:31:21 +0800

    Add gpg signature

--- apt-download.sh
+++ apt-download.sh
@@ -4,35 +4,19 @@ rm -rf /tmp/debs
 mkdir -p /tmp/debs
 cd /tmp/debs
 
-apt-get install --assume-no $1 > apt-get-install.log
-first_line=`grep -n "The following additional packages will be installed:" apt-get-install.log |awk -F":" '{print $1}'`
-first_line=$[first_line+1]
-last_line=`grep -n "Suggested packages:" apt-get-install.log |awk -F":" '{print $1}'`
-last_line=$[last_line-1]
-sed -n "${first_line},${last_line} p" apt-get-install.log > download.log
+echo "##### Install packages $1 to generate version list #####"
+apt-get install -y $1 > apt-get-install.log
 
-first_line=`grep -n "The following NEW packages will be installed:" apt-get-install.log |awk -F":" '{print $1}'`
-first_line=$[first_line+1]
-last_line=`grep -n "The following packages will be upgraded:" apt-get-install.log |awk -F":" '{print $1}'`
-if [ -z "$last_line" ]; then
-  last_line=`grep -n " newly installed, " apt-get-install.log |awk -F":" '{print $1}'`
-fi
-last_line=$[last_line-1]
-sed -n "${first_line},${last_line} p" apt-get-install.log >> download.log
-
-deb_packages=`cat download.log`
-
-# remove the "|" string in the content
-#deb_packages=${deb_packages//|/}
+echo "##### The following packages will be download #####"
+cat apt-get-install.log |grep Unpack |awk '{print $2"="$3}' |awk -F'(' '{print $1$2}' |awk -F')' '{print $1}'
 
 echo "##### Download packages for $1 #####"
-echo "##### apt-get install return message begin #####"
-cat apt-get-install.log |grep -v 'Abort.'
-echo "##### apt-get install return message end #####"
-echo "##### The following packages will be download #####"
-echo ${deb_packages}
-apt-get download ${deb_packages}
+for package in `cat apt-get-install.log |grep Unpack |awk '{print $2"="$3}' |awk -F'(' '{print $1$2}' |awk -F')' '{print $1}'`
+do
+  apt-get download $package ;
+done
+
 echo "###### Downloading finished #####"
-rm -f apt-get-install.log download.log
+rm -f apt-get-install.log
 
 cd ${current_path}
